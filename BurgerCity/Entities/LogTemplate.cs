ca401a93255f6e99039084574b8159b2487ee2e4
Radu Todor <todorradu@gmail.com>
2019-04-13 13:28:27 +0300

    Logger service added using the singleton pattern

---
+++ BurgerCity/Entities/LogTemplate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BurgerCity.Entities
+{
+    public class LogTemplate
+    {
+        private String Date { get; set; }
+        private String Time { get; set; }
+        private String Message { get; set; }
+        private String Exception { get; set; }
+
+        public LogTemplate(String message)
+        {
+            Date = DateTime.Now.ToString("d");
+            Time = DateTime.Now.ToString("T");
+            Message = message;
+            Exception = String.Empty;
+        }
+
+        public LogTemplate(String message, String exception) : this(message)
+        {
+            Exception = exception;
+        }
+
+        public override string ToString()
+        {
+            if (String.IsNullOrEmpty(Exception))
+            {
+                return string.Format(
+                    "--------------------\n" +
+                    "Date: {0}\n" +
+                    "Time: {1}\n" +
+                    "Message: {2}\n" +
+                    "--------------------\n",
+                    Date, Time, Message);
+            }
+            else
+            {
+                return string.Format(
+                    "--------------------\n" +
+                    "Date: {0}\n" +
+                    "Time: {1}\n" +
+                    "Exception Message: {2}\n" +
+                    "Exception StackTrace: {3}\n" +
+                    "--------------------\n",
+                    Date, Time, Message, Exception);
+            }
+        }
+    }
+}
---
+++ BurgerCity/Services/LoggerSingleton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using BurgerCity.Entities;
+
+namespace BurgerCity.Services
+{
+    public sealed class LoggerSingleton
+    {
+        private static LoggerSingleton logger;
+        private static readonly object padlock = new object();
+        private static StreamWriter currentLogFile;
+        private static string currentLogPath;
+
+        private LoggerSingleton()
+        {
+            var currentLogDirectoryPath = string.Format(ConfigurationManager.AppSettings["LogsLocalPath"],
+                System.AppContext.BaseDirectory);
+            currentLogPath = currentLogDirectoryPath +
+                                 string.Format(ConfigurationManager.AppSettings["LogsFileNameFormat"],
+                                     DateTime.Now.ToString("yy-MM-dd"));
+            Directory.CreateDirectory(currentLogDirectoryPath);
+            File.CreateText(currentLogPath).Close();
+        }
+
+        public static LoggerSingleton GetLogger
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (logger == null)
+                    {
+                        logger = new LoggerSingleton();
+                    }
+                    return logger;
+                }
+
+            }
+        }
+
+        public void LogMessage(string message)
+        {
+            using (currentLogFile = new StreamWriter(currentLogPath, true))
+            {
+                currentLogFile.WriteAsync(new LogTemplate(message).ToString());
+            }
+        }
+
+        public void LogError(Exception exception)
+        {
+            using (currentLogFile = new StreamWriter(currentLogPath, true))
+            {
+                currentLogFile.WriteAsync(new LogTemplate(exception.Message, exception.StackTrace).ToString());
+            }
+        }
+    }
+}
